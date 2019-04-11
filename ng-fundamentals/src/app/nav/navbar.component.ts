a76b297e48bdd3b0be8413d49226acb59f21a1b8
DESKTOP-ACT9QHL\Glenn <glenn@glennpacker.net>
2019-04-11 12:55:37 +0100

    navbar styling

--- ng-fundamentals/src/app/nav/navbar.component.ts
+++ ng-fundamentals/src/app/nav/navbar.component.ts
@@ -2,6 +2,21 @@ import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "navbar",
-  templateUrl: "./navbar.component.html"
+  templateUrl: "./navbar.component.html",
+  styles: [
+    `
+      .nav.navbar-nav {
+        font-size: 15px;
+      }
+      #searchForm {
+        margin-right: 100px;
+      }
+      @media (max-width: 1200px) {
+        #searchForm {
+          display: none;
+        }
+      }
+    `
+  ]
 })
 export class NavbarComponent {}
